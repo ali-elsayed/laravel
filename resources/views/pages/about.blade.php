d117a52d39c17cea8be4061f11f7c90de543bd09
EngAli Elsayed <64008k@gmail.com>
2019-10-27 02:57:57 +0200

    first Commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+
+@section('content')
+	<h1>Welcome to about page</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae earum quod aliquid nulla ab obcaecati aut quae, illum voluptates maxime.</p>
+@endsection
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+
+@section('content')
+	<h1>{{$title}}</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae earum quod aliquid nulla ab obcaecati aut quae, illum voluptates maxime.</p>
+@endsection
